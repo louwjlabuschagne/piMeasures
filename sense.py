2be7c7da32849cd2d9c13fbcf54184c415e6912c
Louwrens Labuschagne <louwjlabuschagne@gmail.com>
2017-08-22 19:10:30 +0200

    integrated sense and sheets

--- sense.py
+++ sense.py
@@ -1,4 +1,7 @@
 from sense_hat import SenseHat
+import time
+
+from sheets import writeTemp
 
 sense = SenseHat()
 sense.clear()
@@ -6,10 +9,17 @@ sense.clear()
 tmax = 32
 tmin = 8
 
+time_seconds = 10
+
 while True:
+	time.sleep(time_seconds)
+
     temp = sense.get_temperature()
+    writeTemp(temp)    
     print(temp)
+
     temp = int(temp/4) - tmin
+
     for x in range(0, 8):
         for y in range(0, temp):
             sense.set_pixel(x, y, 255, 0, 0)
--- sheets.py
+++ sheets.py
@@ -1,11 +1,16 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import time
 
-scope = ["https://spreadsheets.google.com/feeds"]
-cred = ServiceAccountCredentials.from_json_keyfile_name('apis/piMeasures-9539a6a7828c.json', scope)
-client = gspread.authorize(cred)
+def writeTemp(temp):
+ 	scope = ["https://spreadsheets.google.com/feeds"]
+	cred = ServiceAccountCredentials.from_json_keyfile_name('apis/piMeasures-9539a6a7828c.json', scope)
+	client = gspread.authorize(cred)
 
-sheet = client.open('piMeasuresSheet').sheet1
+	sheet = client.open('piMeasuresSheet').sheet1
 
-measures = sheet.get_all_records()
-print(measures)
+	results = sheet.get_all_records()
+
+	indx = len(results)
+
+	row = [str(time.asctime()), temp]
